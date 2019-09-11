225efdb10cb12046dc41132f7c814741ab744c1e
Anthony Eckman <anthonyeckman@gmail.com>
2019-09-10 21:30:05 -0500

    Added Archer

---
+++ TacticsPractice/Assets/Scripts/Archer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : PlayerMove
+{
+
+    public GameObject arrow;
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+
+        Debug.DrawRay(transform.position, transform.forward);
+
+        if (!turn)
+        {
+            return;
+        }
+
+        if (!moving && !hasMoved)
+        {
+            FindSelectableTiles();
+            CheckMouse();
+        }
+        else if (!hasMoved)
+        {
+            Move();
+        }
+        else
+        {
+            CheckMouse();
+        }
+
+    }
+
+
+
+    public new void CheckMouse()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+
+                    if (t.selectable)
+                    {
+                        MoveToTile(t);
+                    }
+                }
+                if (hit.collider.tag == "NPC")
+                {
+                    Debug.Log("Attacking Enemy");
+                    if (InRange(hit.collider.gameObject) && !hasAttacked)
+                    {
+                        Attack(hit.collider.gameObject);
+
+                    }
+                    else
+                    {
+                        Debug.Log("Target Out Of Range");
+                    }
+                }
+            }
+        }
+    }
+    
+    private void Attack(GameObject target)
+    {
+        GameObject firedArrow = Instantiate(arrow, transform.position, Quaternion.identity) as GameObject;
+        firedArrow.GetComponent<ArrowScript>().parent = gameObject.GetComponent<TacticsMove>();
+        firedArrow.GetComponent<ArrowScript>().endPos = target.transform.position;
+        firedArrow.GetComponent<ArrowScript>().target = target.GetComponent<TacticsMove>();
+
+        
+
+
+    }
+}
---
+++ TacticsPractice/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public Vector3 startPos;
+
+    public Vector3 endPos;
+
+    public TacticsMove parent;
+
+    public TacticsMove target;
+
+    private int Height = 3;
+
+    private float Incrementor = 0;
+
+    private bool StartThrow = false;
+
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+
+        if(target != null)
+        {
+            StartThrow = true;
+        }
+        if (StartThrow)
+        {
+
+            //if start throw is set active from the move method, it will transform the chesspiece to the target locations
+            Incrementor += 0.04f;
+            Vector3 currentPos = Vector3.Lerp(startPos, endPos, Incrementor);
+            currentPos.y += Height * Mathf.Sin(Mathf.Clamp01(Incrementor) * Mathf.PI);
+            transform.position = currentPos;
+        }
+        //once the chess piece reaches its final destination it will turn start throw back to off and end the movment and reset it.
+        if (transform.position == endPos)
+        {
+            target.TakeDamage(2);
+            BetterTurnManager.EndTurn();
+            Destroy(gameObject);
+
+
+        }
+    }
+
+}
